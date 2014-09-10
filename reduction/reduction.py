bc9fb10c1de1741d3ad778e013de640e228a7e90
Gustavo Banegas <g.banegas@inf.ufsc.br>
2014-09-10 14:20:59 -0300

    add

--- reduction/reduction.py
+++ reduction/reduction.py
@@ -15,13 +15,27 @@ class Reduction(object):
 
     def reduction(self,exp):
         exp_sorted = sorted(exp, reverse=True)
-        mdegree = exp_sorted[0]
+        self.mdegree = exp_sorted[0]
         self.max_collum = (2*exp_sorted[0])-1
         nr = self.calcNR(exp_sorted)
         matrix = self.generateMatrix()
-        for r in matrix:
-            print ''.join(str(r))
-        
+       # self.printMatrix(matrix)
+        exp_sorted.remove(self.mdegree)
+        for i in range(0,len(exp_sorted)):
+            self.reduceFirst(matrix, exp_sorted[i])
+        self.printMatrix(matrix)
+
+
+
+    def reduceFirst(self, matrix, exp):
+        index = self.max_collum-1;
+        #self.printMatrix(matrix)
+        row = [-1 for x in xrange(self.max_collum)]
+        for j in range(0,self.mdegree-1):
+            element = matrix[1][j]
+            row[index - exp] = element
+            index = index -1            
+        matrix.append(row) 
 
     def calcNR(self, exp_sorted):
         nr = 2
@@ -33,8 +47,12 @@ class Reduction(object):
     def generateMatrix(self):
         row = sorted(list(range(0, self.max_collum)), reverse=True)
         #row2 = [-1 for x in xrange(self.max_collum)]
-        matrix = [[]]
+        matrix = [[]] 
         matrix.append(row)
         #matrix.append(row2)
         return matrix
-        
+
+    def printMatrix(self,matrix):
+        for r in matrix:
+            print ''.join(str(r))
+
