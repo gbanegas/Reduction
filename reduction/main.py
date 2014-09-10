5e41ddb36e0cf9070b03a625e42334039a5f66bb
Gustavo Banegas <g.banegas@inf.ufsc.br>
2014-09-10 16:32:08 -0300

    add class polynomial

--- reduction/main.py
+++ reduction/main.py
@@ -4,8 +4,20 @@ Created on 10 Sep 2014
 @author: gustavo
 '''
 from reduction import Reduction
+from polynomial import Polynomial
 
 if __name__ == '__main__':
-    red = Reduction()
-    p = [16,8,0]
-    red.reduction(p)
+	degree = 32
+	f = open('list_' + str(degree) + '.txt','r')
+	pol = Polynomial('x^32 + x^12 + x^1 + 1')
+	print pol
+	#pols = []
+	#for line in f:
+	#	pol = Polynomial(line)
+	#	pols.append(pol)
+
+	#print pols
+	
+	red = Reduction()
+	p = [16,8,0]
+	red.reduction(p)
