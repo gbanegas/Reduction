6defc8ccc688a4c2445422c135c49e9774b31bb2
Gustavo Banegas <g.banegas@inf.ufsc.br>
2014-09-10 13:49:16 -0300

    creat some methods

--- reduction/main.py
+++ reduction/main.py
@@ -3,9 +3,8 @@ Created on 10 Sep 2014
 
 @author: gustavo
 '''
-
-
 from reduction import Reduction
+
 if __name__ == '__main__':
     red = Reduction()
     p = [16,8,0]
--- reduction/reduction.py
+++ reduction/reduction.py
@@ -4,16 +4,37 @@ Created on 10 Sep 2014
 @author: gustavo
 '''
 
+import math
+import numpy
+
+NULL = -1
+max_collum = 0
+mdegree = 0;
+
 class Reduction(object):
-    '''
-    classdocs
-    '''
-
-
-    def __init__(self, params):
-        self.exp = [];
-        
 
     def reduction(self,exp):
+        exp_sorted = sorted(exp, reverse=True)
+        mdegree = exp_sorted[0]
+        self.max_collum = (2*exp_sorted[0])-1
+        nr = self.calcNR(exp_sorted)
+        matrix = self.generateMatrix()
+        for r in matrix:
+            print ''.join(str(r))
         
+
+    def calcNR(self, exp_sorted):
+        nr = 2
+        temp = (exp_sorted[0]+1)/2
+        deg = math.floor(temp)
+        if exp_sorted[1] > deg:
+            nr = 2* (exp_sorted[0] + 1) - exp_sorted[0]
+
+    def generateMatrix(self):
+        row = sorted(list(range(0, self.max_collum)), reverse=True)
+        #row2 = [-1 for x in xrange(self.max_collum)]
+        matrix = [[]]
+        matrix.append(row)
+        #matrix.append(row2)
+        return matrix
         
