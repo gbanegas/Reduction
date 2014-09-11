e96fe5e08c539c7fd5f0951907da247ad05dab2b
Vader@sith <gustavosouzabanegas@gmail.com>
2014-09-10 22:47:24 -0300

    create class to otimization matrix

---
+++ reduction/otimization.py
@@ -0,0 +1,13 @@
+'''
+Created on 10 Sep 2014
+
+@author: gustavo
+'''
+
+import math
+
+class Otimization(object):
+
+	def otimize(self, matrix):
+		#TODO How to doooooooo?????? Create a method
+	print "HELLO"
