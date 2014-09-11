18a758e8e4e4885a98b08e68a852f6379eefb412
Vader@sith <gustavosouzabanegas@gmail.com>
2014-09-11 00:30:41 -0300

    add files

--- reduction/main.py
+++ reduction/main.py
@@ -5,22 +5,30 @@ Created on 10 Sep 2014
 '''
 from reduction import Reduction
 from polynomial import Polynomial
+from threadc import ThreadCount
 
-
+import threading
 if __name__ == '__main__':
+	lock = threading.Lock()
 	degree = 113
 	f = open('list_' + str(degree) + '.txt','r')
+	save = open('result_' + str(degree) + '.txt','w')
 	pols = []
 	for line in f:
 		pol = Polynomial(line)
 		pols.append(pol)
 
-
 	dic = { }
-	red = Reduction()
+	fn = len(pols)/2
+	t1 = ThreadCount(1,lock, pols[0:fn], dic)
+	t2 = ThreadCount(2,lock, pols[fn:len(pols)-1], dic)
+	t1.start()
+	t2.start()
+	t1.join()
+	t2.join()
+#	red = Reduction()
 #	print red.reduction([16,12,8,4,0])
-	for i in pols:
-		dic[i] = red.reduction(i.coefs())
 	for i in dic:
-		print str(i.coefs()) + " xors: " + str(dic[i])
+		r = str(i.coefs()) + " xors: " + str(dic[i])
+		save.write(r + '\n')
 	
--- reduction/threadc.py
+++ reduction/threadc.py
@@ -3,16 +3,29 @@ Created on 10 Sep 2014
 
 @author: gustavo
 '''
+import threading
 from reduction import Reduction
 
-class ThreadCount():
+class ThreadCount(threading.Thread):
 
-	def __init__(self, threadID,  lock, polynomials, dicr):
+	def __init__(self, threadID,  locker, polynomials, dicr):
 		threading.Thread.__init__(self)
 		self.threadID = threadID
-		self.lock = lock
+		self.locker = locker
 		self.polynomials = polynomials
 		self.dicr = dicr
-
 	
+	def run(self):
+		print "Starting thread: " + str(self.threadID) + '\n'
+		for i in self.polynomials:
+			count = red(i.coefs())
+			self.locker.acquire()
+			self.dicr[i] = count
+			self.locker.release()
+
+
+def red(i):
+	reduc = Reduction()
+	count = reduc.reduction(i)
+	return count
 
