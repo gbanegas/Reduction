0e4f3681a0ec0718d8d188a32812a9ff5de82585
Vader@sith <gustavosouzabanegas@gmail.com>
2014-09-10 22:50:01 -0300

    Add the xlsxsave

---
+++ reduction/xlsx.py
@@ -0,0 +1,13 @@
+'''
+Created on 10 Sep 2014
+
+@author: gustavo
+'''
+
+class Xslxsaver(object):
+	def save(self, exp, matrix):
+		#TODO to save in an xlsx
+		print "Saving..."
+
+	def close(self):
+		# TODO Close the file xlsx
