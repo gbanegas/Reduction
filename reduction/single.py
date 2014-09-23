e6dbde99092052044e12e9a1148d489eaf0dd927
Gustavo Banegas <g.banegas@inf.ufsc.br>
2014-09-22 21:46:38 -0300

    ad

--- reduction/single.py
+++ reduction/single.py
@@ -9,30 +9,30 @@ import os
 if __name__ == '__main__':
     degree = 163
     directory = str(degree)
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-    f = open('pent_list_' + str(degree) + '.txt','r')
-    toSave = directory + '/pent_result_' + str(degree) + '.txt'
-    save = open(toSave,'w')
-    pols = []
-    for line in f:
-        pol = Polynomial(line)
-        pols.append(pol)
-   
-    for i in pols:
-        red = Reduction()
-        count = red.reduction(i.coefs())
-        del red
-        result = str(i.coefs()) + " : " + str(count) + "\n"
-        save.write(result)
-        print result
-    
+    #if not os.path.exists(directory):
+    #    os.makedirs(directory)
+    #f = open('pent_list_' + str(degree) + '.txt','r')
+    #toSave = directory + '/pent_result_' + str(degree) + '.txt'
+    ##save = open(toSave,'w')
+    #pols = []
+    #for line in f:
+    #    pol = Polynomial(line)
+    #    pols.append(pol)
+
+    #for i in pols:
+    #    red = Reduction()
+     #   count = red.reduction(i.coefs())
+     #   del red
+     #   result = str(i.coefs()) + " : " + str(count) + "\n"
+     #   save.write(result)
+     #   print result
+
     #save.close()
-    #pol = Polynomial('x^32 + x^31 + 1')
-    #red = Reduction()
-    #count = red.reduction(pol.coefs())
-    #print count
-    
+    pol = Polynomial('x^409 + x^322 + 1')
+    red = Reduction()
+    count = red.reduction(pol.coefs())
+    print count
+
     #print count
 #    after = hp.heap()
 #    leftover = after - before
