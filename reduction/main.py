cc6f5ab325023a5c5c3ec9c45fabbeec9ef093a2
Gustavo <vader@debian.deathstar>
2015-04-16 20:59:34 -0300

    add

--- reduction/main.py
+++ reduction/main.py
@@ -10,31 +10,31 @@ import os
 
 import threading
 if __name__ == '__main__':
-	#lock = threading.Lock()
-	#lockScreen = threading.Lock()
-	#degree = 163
-	#directory = str(degree)
-	# if not os.path.exists(directory):
-	# 	os.makedirs(directory)8
-	# f = open('pol_' + str(degree) + '_.txt','r')
-	# save = open('result_' + str(degree) + '.txt','w')
-	# pols = []
-	# for line in f:
-	# 	pol = Polynomial(line)
-	# 	pols.append(pol)
+	lock = threading.Lock()
+	lockScreen = threading.Lock()
+	degree = 163
+	directory = str(degree)
+	if not os.path.exists(directory):
+	 	os.makedirs(directory)
+	f = open('pol_' + str(degree) + '_.txt','r')
+	save = open('result_' + str(degree) + '.txt','w')
+	pols = []
+	 for line in f:
+	 	pol = Polynomial(line)
+	 	pols.append(pol)
 
 	
-	# fn = len(pols)/2
-	# fn2 = len(pols)/4
-	# t1 = ThreadCount(1,lockScreen, lock, pols[0:fn2], save)
-	# t2 = ThreadCount(2,lockScreen, lock, pols[fn2:fn], save)
-	# t3 = ThreadCount(3,lockScreen, lock, pols[fn:len(pols)-1], save)
-	# t1.start()
-	# t2.start()
-	# t3.start()
-	# t1.join()
-	# t2.join()
-	# t3.join()
+	fn = len(pols)/2
+	fn2 = len(pols)/4
+	t1 = ThreadCount(1,lockScreen, lock, pols[0:fn2], save)
+	t2 = ThreadCount(2,lockScreen, lock, pols[fn2:fn], save)
+	t3 = ThreadCount(3,lockScreen, lock, pols[fn:len(pols)-1], save)
+	t1.start()
+	t2.start()
+	t3.start()
+	t1.join()
+	t2.join()
+	t3.join()
 	red = Reduction()
 	di = dict()
 	#for i in xrange(0,29):
@@ -44,10 +44,10 @@ if __name__ == '__main__':
 #	print di
 
 	#for k in xrange(2,20):
-	#x^283 + x^142 + x^70 + x + 1
+	#x^283 + x^142 + x^70 + x + 
 	resultado = red.reduction([283,142,70,1,0])
-	#di[k] = resultado
+	di[k] = resultado
 	print resultado
-	# if (not t1.isAlive() and t2.isAlive() and t3.isAlive()):
-	# 	save.close();
+	if (not t1.isAlive() and t2.isAlive() and t3.isAlive()):
+	 	save.close();
 	
