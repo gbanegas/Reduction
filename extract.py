d1e4b84cfb06adf3275a45c48a88dc98ba0a7673
Gustavo <vader@debian.deathstar>
2015-05-30 18:17:02 -0300

    added the extract

--- extract.py
+++ extract.py
@@ -1,21 +1,28 @@
 import os
-from collections import defaultdict
+from collections import defaultdict,OrderedDict
 
 import xlsxwriter
 
 
 if __name__ == '__main__':
-    f = open('saida_irre_19.txt','r')
+    f = open('saida_irred_pent_233.txt','r')
     pols = defaultdict(list)
     
     for line in f:
         splited = line.split(':')
         number = splited[1].replace('\n','')
 
-        if pols[int(number)] == None:
+        if pols[int(number)] == None:   
             pols[int(number)] = []
-        pols[int(number)].append(splited[0])
+
+        spl = splited[0].replace(",","").replace("[","").replace("]","").split(' ')
+        numbers = []
+        for i in spl:
+            numbers.append(int(i))
+
         
+        pols[int(number)].append(numbers)
+
             
             #pols.sort(reverse=True)
 
@@ -24,16 +31,24 @@ if __name__ == '__main__':
     name = "" + name
     workbook = xlsxwriter.Workbook(name)
     worksheet = workbook.add_worksheet("irredutiveis")
-    row = 0
+    worksheet.write(0,0, "XORs")
+    worksheet.write(0,1, "m")
+    worksheet.write(0,2, "a")
+    worksheet.write(0,3, "b")
+    worksheet.write(0,4, "c")
+    worksheet.write(0,5, "d")
+    row = 1
     colum = 0
-    for i in pols:
-        colum = 1
-        worksheet.write(row,colum, i)
-        colum = 2
+    f = sorted(pols.keys())
+    for i in f:
         for j in pols[i]:
-            worksheet.write(row,colum, j)
-            colum = colum+1
-        row = row + 1    
+            colum = 0
+            worksheet.write(row,colum, i)
+            colum = 1
+            for num in j:
+                worksheet.write(row,colum, num)
+                colum = colum+1
+            row = row + 1    
 
     workbook.close()
     
