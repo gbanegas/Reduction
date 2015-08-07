8e437308724d2fa2c1039ff9c595c7c245b40fc7
unknown <banegas@BRY-DESKTOP-34.Bry.lan.com.br>
2015-08-07 08:35:58 -0300

    added

--- single.py
+++ single.py
@@ -6,6 +6,36 @@ import os
 from collections import defaultdict
 import sys, getopt
 
+def recoverfile(saved, readed):
+    if not os.path.exists(saved):
+        return True, []
+    f = open(saved,'r')
+    if(not os.stat(saved).st_size==0):
+        pols = []
+        pols_done = []
+        for line in readed:
+            pol = Polynomial(line)
+            pols.append(pol)
+        for line in f:
+            line = line.replace("[","")
+            line = line.replace("]","")
+            spl = line.split(',')
+            p = ""
+            for i in xrange(0,len(spl)-1):
+                p = p + " + x^" + str(spl[i].replace(" ",""))
+            p = p + " + 1"
+            p = p.replace("+","",1)
+            #print p
+            pol_ = Polynomial(p)
+            pols_done.append(pol_)
+
+        pols_set = set(pols)
+        pols_set_done = set(pols_done)
+        result = pols_set - pols_set_done
+        return False, list(result)
+    else:
+        return True, []
+
 def main(argv):
     inputfile = ''
     outputfile = ''
@@ -30,11 +60,21 @@ def main(argv):
         print 'main.py -i <inputfile> -o <outputfile>'
         sys.exit(2)
     l = []
-    for i in fi:
-        p = Polynomial(i)
-        l.append(p)
+    files = [inputfile]
+    for fileName in files:
+        save = outputfile
+        f = open(fileName,'r')
+        read, pols = recoverfile(save, f)
+        if read:
+            for line in f:
+                try:
+                    pol = Polynomial(line)
+                    pols.append(pol)
+                except Exception as e:
+                    print line
+                    sys.exit(2)
     result = defaultdict(list)
-
+    print len(pols)
     for pol in l:
         if len(pol.coefs()) > 1:
             red = Reduction()
